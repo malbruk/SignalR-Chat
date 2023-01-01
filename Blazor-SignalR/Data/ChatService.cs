6b580cb10387b01fa35e8c53cecc9af7c8b11d28
Malka Bruk <malka2039@gmail.com>
2023-01-01 08:41:14 +0200

    fix warnings

---
+++ Blazor-SignalR/Data/ChatService.cs
@@ -0,0 +1,26 @@
+﻿namespace Blazor_SignalR.Data
+{
+    public class ChatService
+    {
+        public Dictionary<string, String> Users { get; set; }
+
+
+        public ChatService()
+        {
+            Users = new Dictionary<string, String>();
+        }
+
+        public void AddUser(string? connectionId, string? userName)
+        {
+            if (string.IsNullOrEmpty(connectionId) || string.IsNullOrEmpty(userName))
+                throw new Exception("ConnectionId and UserName must have a value");
+
+            Users.Add(connectionId, userName);
+        }
+
+        public void RemoveUser()
+        {
+
+        }
+    }
+}
